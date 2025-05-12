6d53feb2d7803c6baa652a2d0acecf72e1429c1a
T. Korn <tobias.korn@towerswatson.com>
2025-05-12 15:21:58 +0200

    added npm package (starting development of UJMW-Client for TS)

---
+++ js/src/ujmwClient.ts
@@ -0,0 +1,6 @@
+
+export class UjmwClient {
+  
+  //TODO_RWE: tolle, großartige, neue, stabile, smarte, gutriechende, ballastfrei, imponierende, rasendschnelle, gaumenverwöhnende variante in TS
+
+}
